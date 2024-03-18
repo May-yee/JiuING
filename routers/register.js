af6f1971d312a1d7f6086bf4ac773b3092a082de
JAI-KAI <good003047@gmail.com>
2024-03-18 14:45:29 +0800

    password point

--- routers/register.js
+++ routers/register.js
@@ -1,11 +1,17 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 var multer = require('multer');
 var crypto = require("crypto");
 const algorithm = 'aes-256-cbc';
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
-
+var session = require("express-session");
+app.use(session({
+    secret: "Pa$$w0rd",
+    resave: true,
+    saveUninitialized: true
+}));
 
 var myStorage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -19,6 +25,7 @@ var upload = multer({
     storage: myStorage,  // 設置 storage
 });
 var mysql = require("mysql");
+const exp = require('constants');
 var conn = mysql.createConnection({
     user: "root",
     password: "",
@@ -26,6 +33,7 @@ var conn = mysql.createConnection({
     port: 3306,
     database: "jiuing"
 });
+//註冊
 router.post("/", upload.single('headShot'), function(req,res){
     var cipher = crypto.createCipheriv(algorithm, key, iv);
     let encrypted = cipher.update(req.body.passWord, 'utf8', 'hex');
@@ -39,7 +47,7 @@ router.post("/", upload.single('headShot'), function(req,res){
         }
     )
 });
-
+//登入
 router.post("/login", function(req, res) {
     conn.query("select * from member where userEmail = ?",
         [req.body.userEmail],
@@ -49,7 +57,9 @@ router.post("/login", function(req, res) {
                 let decrypted = decipher.update(rows[0].passWord, 'hex', 'utf8');
                 decrypted += decipher.final('utf8');    //解析密碼
                 console.log(decrypted);
+                req.session.userID = rows[0].memberID;
                 if(decrypted == req.body.passWord) {
+                    console.log(rows[0].memberID);
                     res.send({
                         success: true, 
                         headShot: rows[0].headShot,
